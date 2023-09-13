651705da375b18acf85395694fe1632149fe8e19
shouhei <shohei.nagamura@gmail.com>
2023-09-14 01:50:57 +0900

    Blog一覧画面を作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,15 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import axios from "axios";
+import { Post } from "@/type";
+import Link from "next/link";
+import styles from "@/styles/Home.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
+
+type Props = {
+  posts: Post[];
+};
 
 export async function getStaticProps() {
   const res = await fetch("http://localhost:3001/api/v1/posts");
@@ -18,7 +25,25 @@ export async function getStaticProps() {
   };
 }
 
-export default function Home() {
-  return;
-  <h1></h1>;
+export default function Home({ posts }: Props) {
+  return (
+    <div className={styles.homeContainer}>
+      <h2>Rails & Next.js Blog</h2>
+      <Link href="/create-post" className={styles.createButton}>
+        Create New Post
+      </Link>
+      <div>
+        {posts.map((post: Post) => (
+          <div key={post.id} className={styles.postCard}>
+            <Link href={`posts/@{post.id}`} className={styles.postCardBox}>
+              <h2>{post.title}</h2>
+            </Link>
+            <p>{post.content}</p>
+            <button className={styles.editButton}>編集</button>
+            <button className={styles.deleteButton}>削除</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
